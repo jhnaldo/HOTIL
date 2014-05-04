8cc484c2e676a36a0763fad1858c9ffee305571b
naldo <jhpjhp0223@gmail.com>
2014-05-05 00:12:42 +0900

    [URL] redirect to index page of main

--- HOTIL/HOTIL/urls.py
+++ HOTIL/HOTIL/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
+from django.http import *
 
 urlpatterns = patterns('',
+    (ur'^$', lambda request: HttpResponseRedirect('/main/')),
     (ur'^main/', include('main.urls')),
     (ur'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': './media'}),
 )
