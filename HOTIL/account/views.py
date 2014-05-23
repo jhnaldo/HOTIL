92e54323f652aadeffdc81595027256e02dbd89c
naldo <jhpjhp0223@gmail.com>
2014-05-24 02:42:27 +0900

    [Account] login

--- HOTIL/account/views.py
+++ HOTIL/account/views.py
@@ -1,15 +1,33 @@
 #-*- coding: utf-8-*-
 
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.http import HttpResponseRedirect, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
 
 def signup(request):
     return render_to_response('account/signup.html', {
     }, context_instance=RequestContext(request))
 
+@csrf_exempt
 def _login(request):
-    return render_to_response('account/login.html', {
-    }, context_instance=RequestContext(request))
+    print request.method
+    if request.method == 'POST':
+        user_id = request.POST.get('id','')
+        password = request.POST.get('passwd','')
+        try:
+            user = authenticate(username=user_id, password=password)
+        except UnicodeEncodeError:
+            return HttpResponse(-1)
+        if user is not None: # Login OK
+            login(request, user)
+            return HttpResponse(1)
+        else: # Login Failed
+            return HttpResponse(0)
+    else:
+        return HttpResponseRedirect('/')
 
 def _logout(request):
     return render_to_response('account/login.html.html', {
--- HOTIL/media/js/layout.js
+++ HOTIL/media/js/layout.js
@@ -1,6 +1,71 @@
 function autoHeight(){
-    var height = parseInt($(window).outerHeight() - $('#header').outerHeight() - $('#footer').outerHeight());
+    var window_height = parseInt($(window).outerHeight());
+    var height = parseInt(window_height - $('#header').outerHeight() - $('#footer').outerHeight());
     var container_height = parseInt($('#container').outerHeight());
-    if(container_height < height)
+    if(container_height < height){
         $('#container').css('height', height+'px');
+        $('#background').css('height', window_height+'px');
+    }
+    centerLoginWindow();
+}
+
+function loginDialog(){
+    var body = $('body');
+    var background = $('<div>',{'id':'background'});
+    background.appendTo(body);
+
+    var login_div = $('<div>',{'id':'login'});
+    login_div.appendTo(body);
+
+    var exit = $('<img>',{'src':'/media/image/x.png','id':'exit','onclick':'closeLogin()'});
+    exit.appendTo(login_div);
+
+    var login_content = $('<div>', {'id':'login-content'});
+    login_content.appendTo(login_div);
+    login_content.load('/media/html/login.html');
+
+    centerLoginWindow();
+}
+
+function closeLogin(){
+    $('#background').remove();
+    $('#login').remove();
+}
+
+function centerLoginWindow(){
+    var window_height = parseInt($(window).outerHeight());
+    var header_width = parseInt($('#header').outerWidth());
+    $('#login').css('margin-left', (header_width-500)/2+'px');
+    $('#login').css('margin-top', (window_height-300)/2+'px');
+}
+
+function ajaxLogin(){
+    var id = document.getElementsByName('id')[0].value;
+    var passwd = document.getElementsByName('password')[0].value;
+
+    var data = {'id':id, 'passwd':passwd};
+
+    $.ajax({
+        url: '/login/',
+        type: 'POST',
+        dataType : 'json',
+        data: data,
+        success:function(result){
+            console.log(result);
+            switch(result){
+                case -1:
+                    alert('잘못된 입력입니다.');
+                    break;
+                case 0:
+                    alert('존재하지 않는 아이디 혹은 잘못된 비밀번호입니다.');
+                    break;
+                case 1:
+                    alert('로그인 완료');
+                    break;
+            }
+        },
+        error:function(e){
+            console.log('[ERROR] '+e);
+        }
+    });
 }
